f1c063ca3698c1acfe425134d729c91415051906
anuththarasapumal <anusapumal8@gmail.com>
2023-10-01 21:05:26 -0700

    search_results

--- src/app/pages/call-rates/call-rates.component.ts
+++ src/app/pages/call-rates/call-rates.component.ts
@@ -32,10 +32,11 @@ export class CallRatesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
     this.titleService.setTitle("Vinota Call Rates | Affordable International Calling");
     this.metaService.updateTag({ name: 'keywords', content: 'Vinota Call Rates,Affordable International Calling' });
     this.metaService.updateTag({ name: 'description', content: 'Explore Vinota`s competitive call rates for global connections. Save on  international calls. Enjoy cheap rates and exceptional call quality. Check our rates now.' });
-  
+  // Get call rates
     onValue(ref(this.db, 'full_rate_db/minimum_rate'), (snapshot) => {
       const data = snapshot.val();
       this.countries = [];
@@ -49,9 +50,92 @@ export class CallRatesComponent implements OnInit {
       this.countriesList = this.groupArrayByFirstLetter(this.countries);
     }, { onlyOnce: true });
   }
-  private _filterStates(value: string): any[] {
-    const filterValue = value.toLowerCase();
-    return this.countries.filter((country: any) => country.name.toLowerCase().includes(filterValue));
+
+// Filter countries
+  private _filterStates(value: string) {
+    let filterValue = value.toLowerCase();
+    let filerArray = this.countries.filter((country: any) => country.name.toLowerCase().includes(filterValue));
+        if( filterValue=='ksa'){
+          let obj={
+            "name":'Saudi Arabia',
+            "iso": "sa",
+            "rate":0.0767,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='rok'){
+           let obj={
+            "name":'South Korea',
+            "iso": "kr",
+            "rate": 0.0216,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='usa'){
+          let obj={
+            "name":'United States',
+            "iso": "us",
+            "rate":0.01,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='uk'|| filterValue=='great britain' || filterValue=='britain'|| filterValue=='england'){
+          let obj={
+            "name":'United Kingdom',
+            "iso": "gb",
+            "rate":0.0595,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=="deutschland" || filterValue=="frg"){
+          let obj={
+            "name":'Germany',
+            "iso": "de",
+            "rate": 0.1825,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='italia'){
+          let obj={
+            "name":'Italy',
+            "iso": "it",
+            "rate": 0.01,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='prc'){
+          let obj={
+            "name":'China',
+            "iso": "cn",
+            "rate": 0.216,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='rsa'){
+          let obj={
+            "name":'South Africa',
+            "iso": "za",
+            "rate": 0.1674,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='uae' || filterValue=='abu dhabi'  || filterValue=='dubai' || filterValue=='sharjah' || filterValue=='ajman' || filterValue== 'umm al-quwain' || filterValue=='fujairah'){
+          let obj={
+            "name":'United Arab Emirates',
+            "iso": "ae",
+            "rate": 0.1393,
+          }
+          filerArray.push(obj)
+        }
+        if(filterValue=='drc'){
+          let obj={
+            "name":'Democratic Republic of the Congo',
+            "iso": "cd",
+            "rate": 0.3205,
+          }
+          filerArray.push(obj)
+        }
+        return filerArray
   }
 
   groupArrayByFirstLetter(arr: any) {
